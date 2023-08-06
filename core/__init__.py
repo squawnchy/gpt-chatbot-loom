cce46b967a4948b4dd813ed85c6e8d9528f62c59
Marc <marc-gilbrecht@outlook.de>
2023-08-06 13:27:46 +0000

    refactor: remove reference to removed config class

--- core/__init__.py
+++ core/__init__.py
@@ -1,7 +1,6 @@
 """Core package for the gpt-chatbot-loom"""
 from .bots import ChatBot, ChatBotLoom
-from .config import Config
 from .runner import run_chat_ui
 
-__all__ = ["ChatBot", "ChatBotLoom", "Config", "run_chat_ui"]
+__all__ = ["ChatBot", "ChatBotLoom", "run_chat_ui"]
 __version__ = "0.1.0"

f3ac0e07ba112da95306a7629a1d5cda14503d55
Marc <marc-gilbrecht@outlook.de>
2023-08-06 03:18:34 +0000

    feat: add initial runner

---
+++ core/runner.py
@@ -0,0 +1,8 @@
+"""Runs UI for interacting with a chatbot."""
+import gradio as gr
+from .bots import ChatBot
+
+
+def run_ui(bot: ChatBot):
+    """Runs the UI for the given chatbot."""
+    gr.ChatInterface(bot.chat).launch()

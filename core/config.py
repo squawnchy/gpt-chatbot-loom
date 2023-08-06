801ca4ae7ee87687dfb5d9eff584a416f25c9173
Marc <marc-gilbrecht@outlook.de>
2023-08-06 13:23:59 +0000

    chore: remove unused config class

--- core/config.py
+++
@@ -1,9 +0,0 @@
-"""config module."""
-import os
-
-
-class Config:
-    """config set from environment variables."""
-
-    OPENAI_API_KEY: str = os.getenv("OPENAI_API_KEY")
-    BOTS_FILE: str = os.getenv("BOTS_FILE")

11e9019ec2e10974f609a28cfdee97bc940ea618
Marc <marc-gilbrecht@outlook.de>
2023-08-06 13:21:14 +0000

    feat: adjust runner

--- core/runner.py
+++ core/runner.py
@@ -6,7 +6,6 @@ from .bots import ChatBot
 def run_chat_ui(bot: ChatBot):
     """Runs the UI for the given chatbot."""
 
-    # the function to wrap the chat interface around. Should accept two parameters: a string input message and list of two-element lists of the form [[user_message, bot_message], ...] representing the chat history, and return a string response. See the Chatbot documentation for more information on the chat history format.
     def chat(message, history):
         return bot.chat(message)
 
@@ -19,5 +18,5 @@ def run_chat_ui(bot: ChatBot):
         clear_btn=None,
         title=bot.name,
         description=bot.description,
-        chatbot=gr.Chatbot(height=500),
+        chatbot=gr.Chatbot(height=480),
     ).launch()
